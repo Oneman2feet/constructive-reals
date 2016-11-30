db842af9b9698f8addc99864de899f255b612960
Zachary Zimmerman <Oneman2feet@gmail.com>
2016-11-30 15:47:47 -0500

    cleaned

--- reals.ml
+++ reals.ml
@@ -1,28 +1,13 @@
 #require "zarith";;
 
+(* Faster bignum arithmetic *)
 let ( * ) = Z.mul;;
 let ( + ) = Z.add;;
 let ( - ) = Z.sub;;
 let ( / ) = Z.div;;
-
-(* Integers *)
 let (~$) = Z.of_int;;
-print_string (Z.to_string (Z.mul ~$5 ~$10));;
 
-print_string "\n";;
-
-(* Rationals *)
-print_string (Q.to_string (Q.of_ints 3 2));;
-
-print_string "\n";;
-
-(* Convenience methode for Integers *)
-(*
-let rec factorial x = match x with
-  | Z.zero -> Z.one
-  | _ -> Z.mul x (Z.pred x)
-*)
-
+(* Convenience methods for Integers *)
 let rec factorial x = if Z.leq x Z.zero then Z.one else
   Z.mul x (factorial (Z.pred x));;
 
@@ -40,14 +25,15 @@ module R =
       let of_int x = function n -> Q.of_int x
       let of_bigint x n = Q.of_bigint x
       let to_string x n = Q.to_string (x n)
+      (* TODO: implement long division with bignums for more precision *)
       let to_float x n = (Z.to_float (Q.num (x n))) /. (Z.to_float (Q.den (x n)))
       let print_decimal x n = print_float (to_float x n)
     end;;
 
-print_string (R.to_string (R.of_int 5) (Z.of_int 100));;
+print_string (R.to_string (R.of_int 5) ~$100);;
 
 print_string "\n";;
 
-R.print_decimal R.e (Z.of_int 100);;
+R.print_decimal R.e ~$100;;
 
 print_string "\n";;
